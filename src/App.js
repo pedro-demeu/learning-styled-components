6b822b3e4edefea91151af2b9615f0b9392cd9b8
pedro <pdemeu@outlook.com>
2021-10-14 08:34:54 -0300

    Exportando estilos Globais do meu css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobalStyles } from "./Components/GlobalStyles";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Cabecalho />
       <Container />
     </>
---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+//Exportando estilos Globais do meu css
+export const GlobalStyles = createGlobalStyle
+`
+    * {
+        box-sizing: border-box;
+        font-family: "Montserrat", sans-serif;
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        color: grey;
+    }
+`
